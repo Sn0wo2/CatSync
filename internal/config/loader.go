1a7e39665920d4c1de72daa9619ce4e987cefde5
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-08-05 18:43:56 +0800

    refactor(config): loader
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- internal/config/loader.go
+++ internal/config/loader.go
@@ -10,23 +10,23 @@ import (
 )
 
 func Init(loaders ...Loader) error {
+	var err error
+	Instance, err = NewConfig(loaders...)
+	return err
+}
+
+func NewConfig(loaders ...Loader) (*Config, error) {
 	loaderByExt := make(map[string]Loader)
-	var allowedExts []string
-	{
-		allowedExtsSet := make(map[string]struct{})
-		for _, l := range loaders {
-			for _, ext := range l.GetAllowFileExtensions() {
-				dottedExt := "." + strings.ToLower(ext)
-				loaderByExt[dottedExt] = l
-				allowedExtsSet[dottedExt] = struct{}{}
-			}
-		}
-		for ext := range allowedExtsSet {
-			allowedExts = append(allowedExts, ext)
+	for _, l := range loaders {
+		for _, ext := range l.GetAllowFileExtensions() {
+			loaderByExt["."+strings.ToLower(ext)] = l
 		}
 	}
 
-	var foundPath string
+	allowedExts := make([]string, 0, len(loaderByExt))
+	for ext := range loaderByExt {
+		allowedExts = append(allowedExts, ext)
+	}
 
 	envPath := os.Getenv("CONFIG_PATH")
 	if debug.IsDebugging() {
@@ -35,12 +35,15 @@ func Init(loaders ...Loader) error {
 		}
 	}
 
+	var foundPath string
+
 	if envPath != "" {
 		if _, err := os.Stat(envPath); err == nil {
 			foundPath = envPath
 		} else {
+			base := strings.TrimSuffix(envPath, filepath.Ext(envPath))
 			for _, ext := range allowedExts {
-				tryPath := envPath + ext
+				tryPath := base + ext
 				if _, err := os.Stat(tryPath); err == nil {
 					foundPath = tryPath
 					break
@@ -51,9 +54,6 @@ func Init(loaders ...Loader) error {
 
 	if foundPath == "" {
 		searchPaths := []string{"./data/"}
-		if debug.IsDebugging() {
-			searchPaths = []string{"./"}
-		}
 
 	searchLoop:
 		for _, p := range searchPaths {
@@ -69,26 +69,25 @@ func Init(loaders ...Loader) error {
 
 	if foundPath == "" {
 		if envPath != "" {
-			return fmt.Errorf("config file specified by env var not found: %s", envPath)
+			return nil, fmt.Errorf("config file specified by env var not found: %s", envPath)
 		}
-		return fmt.Errorf("no config file found in search paths")
+		return nil, fmt.Errorf("no config file found in search paths")
 	}
 
 	ext := strings.ToLower(filepath.Ext(foundPath))
 	loader, ok := loaderByExt[ext]
 	if !ok {
-		return fmt.Errorf("unsupported config file extension: %s", ext)
+		return nil, fmt.Errorf("unsupported config file extension: %s", ext)
 	}
 
 	cfg, err := loader.Load(foundPath)
 	if err != nil {
-		return err
+		return nil, fmt.Errorf("failed to load config file %s: %w", foundPath, err)
 	}
 
-	if err = validate(cfg); err != nil {
-		return fmt.Errorf("config validation failed: %w", err)
+	if err := validate(cfg); err != nil {
+		return nil, fmt.Errorf("config validation failed: %w", err)
 	}
 
-	Instance = cfg
-	return nil
+	return cfg, nil
 }
