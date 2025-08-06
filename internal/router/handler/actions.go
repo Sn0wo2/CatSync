af2469c2062acdf1da5d6d59c5139a75976b663c
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-08-06 08:42:17 +0800

    refactor(handler): actions
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

--- internal/router/handler/actions.go
+++ internal/router/handler/actions.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -8,35 +9,42 @@ import (
 
 	"github.com/Sn0wo2/FileSync/internal/action"
 	"github.com/Sn0wo2/FileSync/internal/config"
+	"github.com/Sn0wo2/FileSync/internal/log"
+	"github.com/Sn0wo2/FileSync/internal/util"
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
-func Actions(a config.Action) fiber.Handler {
-	return func(c *fiber.Ctx) error {
-		switch a.Action {
+func Actions(act config.Action) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		switch act.Action {
 		case action.File:
-			safePath, err := filepath.Abs(a.ActionData)
+			safePath, err := filepath.Abs(act.ActionData)
 			if err != nil {
-				return fiber.ErrInternalServerError
+				return fmt.Errorf("failed to get absolute path: %w", err)
 			}
 			wd, err := os.Getwd()
 			if err != nil {
-				return fiber.ErrInternalServerError
+				return fmt.Errorf("failed to get working directory: %w", err)
 			}
-			if !strings.HasPrefix(safePath, wd) {
-				return fiber.ErrForbidden
+			if !strings.HasPrefix(safePath, filepath.Join(wd, "data")) {
+				return fmt.Errorf("file path is not in data directory: %s", safePath)
 			}
 			fileBytes, err := os.ReadFile(safePath)
 			if err != nil {
-				return fiber.ErrNotFound
+				return fmt.Errorf("failed to read file: %w", err)
 			}
-			c.Set(fiber.HeaderContentType, http.DetectContentType(fileBytes))
-			return c.Send(fileBytes)
+			ctx.Set(fiber.HeaderContentType, http.DetectContentType(fileBytes))
+
+			log.Instance.Info("A >> Serving file", zap.String("file", safePath), zap.String("ctx", util.FiberContextString(ctx)))
+			return ctx.Send(fileBytes)
 		case action.String:
-			return c.SendString(a.ActionData)
+			log.Instance.Info("A >> Serving string", zap.String("string", act.ActionData), zap.String("ctx", util.FiberContextString(ctx)))
+			return ctx.SendString(act.ActionData)
 		case action.URL302:
-			return c.Status(fiber.StatusFound).Redirect(a.ActionData)
+			log.Instance.Info("A >> Redirecting to URL", zap.String("url", act.ActionData), zap.String("ctx", util.FiberContextString(ctx)))
+			return ctx.Status(fiber.StatusFound).Redirect(act.ActionData)
 		}
-		return c.Next()
+		return ctx.Next()
 	}
 }
