aa2bc03ec637b65cefe3268e4d9cac0b57d8bf00
Me0wo <152751263+Sn0wo2@users.noreply.github.com>
2025-12-09 11:54:01 +0800

    feat(params): init
    Signed-off-by: Me0wo <152751263+Sn0wo2@users.noreply.github.com>

---
+++ params/params.go
@@ -0,0 +1,67 @@
+package params
+
+import (
+	"github.com/Sn0wo2/CatSync/config"
+	"go.uber.org/zap"
+)
+
+type Value[T any] struct{ v T }
+
+func (v Value[T]) Get() T { return v.v }
+
+type Ctx struct {
+	vals map[any]any
+}
+
+func New() *Ctx {
+	return &Ctx{vals: make(map[any]any)}
+}
+
+func Set[F any, T any](c *Ctx, field F, val T) *Ctx {
+	if c.vals == nil {
+		c.vals = make(map[any]any)
+	}
+	c.vals[field] = Value[T]{v: val}
+	return c
+}
+
+func Get[F any, T any](c *Ctx, field F) (T, bool) {
+	if c.vals == nil {
+		var zero T
+		return zero, false
+	}
+	raw, ok := c.vals[field]
+	if !ok {
+		var zero T
+		return zero, false
+	}
+	v, ok := raw.(Value[T])
+	if !ok {
+		var zero T
+		return zero, false
+	}
+	return v.Get(), true
+}
+
+func (c *Ctx) GetConfig() *config.Config {
+	cfg, _ := Get[Config, *config.Config](c, Config{})
+	return cfg
+}
+
+func (c *Ctx) SetConfig(cfg *config.Config) *Ctx {
+	return Set[Config, *config.Config](c, Config{}, cfg)
+}
+
+func (c *Ctx) GetLogger() *zap.Logger {
+	logger, _ := Get[Logger, *zap.Logger](c, Logger{})
+	return logger
+}
+
+func (c *Ctx) SetLogger(logger *zap.Logger) *Ctx {
+	return Set[Logger, *zap.Logger](c, Logger{}, logger)
+}
+
+type (
+	Config struct{}
+	Logger struct{}
+)
