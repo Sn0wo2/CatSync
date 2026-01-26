7cc58544f9f0e4396afed9238de9e60e87e1d58a
Me0wo <public@me0wo.cc>
2026-01-26 08:51:42 +0800

    feat(util): regexp
    Signed-off-by: Me0wo <public@me0wo.cc>

---
+++ internal/util/regexp.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"fmt"
+	"regexp"
+	"sync"
+)
+
+var (
+	regexpCache = make(map[string]*regexp.Regexp)
+	cacheMutex  sync.RWMutex
+)
+
+func GetCompiledRegexp(pattern string) (*regexp.Regexp, error) {
+	cacheMutex.RLock()
+	if re, ok := regexpCache[pattern]; ok {
+		cacheMutex.RUnlock()
+		return re, nil
+	}
+	cacheMutex.RUnlock()
+
+	re, err := regexp.Compile(pattern)
+	if err != nil {
+		return nil, fmt.Errorf("invalid regexp %q: %w", pattern, err)
+	}
+
+	cacheMutex.Lock()
+	regexpCache[pattern] = re
+	cacheMutex.Unlock()
+
+	return re, nil
+}
