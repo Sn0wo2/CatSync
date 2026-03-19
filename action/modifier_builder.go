97b6b580f44723cb2a0892a06e37984f8e307caf
Me0wo <public@me0wo.cc>
2026-03-19 23:47:08 +0800

    refactor(action): modifier builder
    Signed-off-by: Me0wo <public@me0wo.cc>

---
+++ action/modifier_builder.go
@@ -0,0 +1,84 @@
+package action
+
+import "github.com/Sn0wo2/CatSync/config"
+
+type ModifierBuilder struct{}
+
+func NewModifierBuilder() *ModifierBuilder {
+	return &ModifierBuilder{}
+}
+
+func (b *ModifierBuilder) BuildGlobal(cfg *config.Config) []Modifier {
+	if cfg == nil {
+		return nil
+	}
+
+	modifiers := make([]Modifier, 0, len(cfg.Modifiers))
+	for i := range cfg.Modifiers {
+		modifiers = append(modifiers, b.BuildFromGlobalModifier(&cfg.Modifiers[i])...)
+	}
+
+	return modifiers
+}
+
+func (b *ModifierBuilder) BuildAction(act *config.Action) []Modifier {
+	if act == nil {
+		return nil
+	}
+
+	return b.BuildFromGlobalModifier(&act.GlobalModifier)
+}
+
+func (b *ModifierBuilder) BuildPayload(data config.ActionData) []Modifier {
+	switch v := data.(type) {
+	case *config.ActionStringData:
+		if v == nil {
+			return nil
+		}
+
+		return b.BuildFromGlobalModifier(&v.GlobalModifier)
+	case *config.ActionFileData:
+		if v == nil {
+			return nil
+		}
+
+		return b.BuildFromGlobalModifier(&v.GlobalModifier)
+	case *config.ActionServerData:
+		if v == nil {
+			return nil
+		}
+
+		return b.BuildFromGlobalModifier(&v.GlobalModifier)
+	case *config.ActionReloadData:
+		if v == nil {
+			return nil
+		}
+
+		return b.BuildFromGlobalModifier(&v.GlobalModifier)
+	default:
+		return nil
+	}
+}
+
+func (b *ModifierBuilder) BuildFromGlobalModifier(gm *config.GlobalModifier) []Modifier {
+	if gm == nil {
+		return nil
+	}
+
+	modifiers := make([]Modifier, 0, 3)
+	if gm.ActionModifierStatus != nil {
+		modifiers = append(modifiers, NewStatusModifier().WithStatus(gm.Status))
+	}
+
+	if gm.ActionModifierAuth != nil {
+		modifiers = append(modifiers, NewAuthModifier(*gm.ActionModifierAuth))
+	}
+
+	if gm.ActionModifierResponseHeader != nil {
+		for k, v := range gm.ActionModifierResponseHeader.Header {
+			modifiers = append(modifiers, NewResponseHeaderModifier(k, v...))
+		}
+	}
+
+	return modifiers
+}
--- router/handler/actions.go
+++ router/handler/actions.go
@@ -5,89 +5,12 @@ import (
 
 	"github.com/Sn0wo2/CatSync/action"
 	"github.com/Sn0wo2/CatSync/action/execute"
-	"github.com/Sn0wo2/CatSync/config"
 	"github.com/Sn0wo2/CatSync/params"
 	"github.com/gofiber/fiber/v3"
 )
 
-type ModifierBuilder struct{}
-
-func NewModifierBuilder() *ModifierBuilder {
-	return &ModifierBuilder{}
-}
-
-func (b *ModifierBuilder) BuildGlobal(cfg *config.Config) []action.Modifier {
-	var modifiers []action.Modifier
-
-	if cfg == nil {
-		return modifiers
-	}
-
-	for i := range cfg.Modifiers {
-		modifiers = append(modifiers, b.BuildFromGlobalModifier(&cfg.Modifiers[i])...)
-	}
-
-	return modifiers
-}
-
-func (b *ModifierBuilder) BuildFromGlobalModifier(gm *config.GlobalModifier) []action.Modifier {
-	var modifiers []action.Modifier
-	if gm == nil {
-		return modifiers
-	}
-
-	if gm.ActionModifierStatus != nil {
-		modifiers = append(modifiers, action.NewStatusModifier().WithStatus(gm.Status))
-	}
-
-	if gm.ActionModifierAuth != nil {
-		modifiers = append(modifiers, action.NewAuthModifier(*gm.ActionModifierAuth))
-	}
-
-	if gm.ActionModifierResponseHeader != nil {
-		for k, v := range gm.ActionModifierResponseHeader.Header {
-			modifiers = append(modifiers, action.NewResponseHeaderModifier(k, v...))
-		}
-	}
-
-	return modifiers
-}
-
-func (b *ModifierBuilder) BuildAction(act *config.Action) []action.Modifier {
-	if act == nil {
-		return nil
-	}
-
-	return b.BuildFromGlobalModifier(&act.GlobalModifier)
-}
-
-func (b *ModifierBuilder) BuildPayload(data config.ActionData) []action.Modifier {
-	switch v := data.(type) {
-	case *config.ActionStringData:
-		if v == nil {
-			return nil
-		}
-
-		return b.BuildFromGlobalModifier(&v.GlobalModifier)
-	case *config.ActionFileData:
-		if v == nil {
-			return nil
-		}
-
-		return b.BuildFromGlobalModifier(&v.GlobalModifier)
-	case *config.ActionServerData:
-		if v == nil {
-			return nil
-		}
-
-		return b.BuildFromGlobalModifier(&v.GlobalModifier)
-	}
-
-	return nil
-}
-
 func Actions(c *params.Ctx) fiber.Handler {
-	b := NewModifierBuilder()
+	b := action.NewModifierBuilder()
 
 	pl, err := execute.Compile(c.GetConfig(), execute.Builders{Global: b.BuildGlobal, Action: b.BuildAction, Payload: b.BuildPayload})
 	if err != nil {
